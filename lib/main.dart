8b7c7fdfece518166102ad38ed5b3e2d5aaa1901
Calvin Das <calvindas7@gmail.com>
2023-05-10 19:44:38 +0530

    🏏Text input in flutter various decoration focused disabled enabled , prefix icons text, sufix, textInputCOntroller

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_first_flutterapp/ui_helper/util.dart';
 
 void main() {
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   var arrNames = ['Calvin', 'Krupa', 'Navya', 'Panda', 'Sinchana'];
+  var email = TextEditingController();
 
   MyHomePage({super.key});
   @override
@@ -35,34 +37,103 @@ class MyHomePage extends StatelessWidget {
           title: const Text("My first Flutter app"),
         ),
         body:Center(
-          child: Container(
-            margin:const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Card(
-                  elevation: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Calvin',
-                      style: Theme.of(context).textTheme.labelLarge,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                margin: EdgeInsets.all(10),
+                child: TextField(
+                  controller: email,
+                decoration: InputDecoration(
+                  // labelText: 'Email',
+                  hintText: 'Email enter',
+                  prefixIcon: Icon(Icons.person),
+                  suffixText: 'So',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: (){
+
+                    },
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 2.0,
                     ),
                   ),
-                ),
-                Card(
-                  elevation: 10,
-                  shadowColor: Colors.deepOrange,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'krupa',
-                      style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.pink),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(
+                      color: Colors.yellow,
+                      width: 2.0,
                     ),
                   ),
+                )
                 ),
-              ],
-            ),
-          ),
+              ),
+              Container(
+                width: 300,
+                margin: EdgeInsets.all(10),
+                child: TextField(
+                  obscureText: true,
+                    keyboardType: TextInputType.number,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 2.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.yellow,
+                          width: 2.0,
+                        ),
+                      ),
+                    )
+                ),
+              ),
+              Container(
+                width: 300,
+                margin: EdgeInsets.all(10),
+                child: TextField(
+                    enabled: false,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.pink,
+                          width: 2.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 2.0,
+                        ),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 2.0,
+                        ),
+                      ),
+                    )
+                ),
+              ),
+              ElevatedButton(onPressed: (){
+                String uEmail = email.text.toString();
+                print('$uEmail');
+              }, child: Text('Continue'))
+            ],
+          )
         ),
     );
   }
